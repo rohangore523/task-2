80bdebf4d1f819af652f0be442a182b0fbfeb4ca
rohangore523 <rohangore523@gmail.com>
2026-02-28 09:10:12 +0530

    Add files via upload

---
+++ TemperatureConverter.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class TemperatureConverter {
+    public static void main(String[] args) {
+
+        double fahrenheit = 98.6;
+        double celsius = (fahrenheit - 32) * 5 / 9;
+
+        System.out.printf("Fixed Value: %.2f°F = %.2f°C\n", fahrenheit, celsius);
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter temperature in Fahrenheit: ");
+        double userFahrenheit = scanner.nextDouble();
+
+        double userCelsius = (userFahrenheit - 32) * 5 / 9;
+
+        System.out.printf("User Input: %.2f°F = %.2f°C", userFahrenheit, userCelsius);
+
+        scanner.close();
+    }
+}
